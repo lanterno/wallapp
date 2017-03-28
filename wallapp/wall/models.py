c323317124fd6a7df5118f661eccc66ce749cff0
zee93 <ahmeds.elghareeb@gmail.com>
2017-03-28 21:04:17 +0200

    Added important dates

--- wallapp/wall/models.py
+++ wallapp/wall/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
 
     owner = models.ForeignKey(User, related_name='posts')
     text = models.CharField(max_length=settings.POST_MAX_LENGTH)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update_date = models.DateTimeField(auto_now=True)
